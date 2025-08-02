2f06c9629c30fbb55eabeb913c68c70c3725b36a
Shakthi <shakthi.jashwanth@gmail.com>
2025-08-02 17:06:51 +0530

    Counter Mini Project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter]=useState(5)
+
+  //let counter=5
+  const addValue=()=>{
+    if (counter>=20){
+      return
+    }
+    counter+=1
+    setCounter(counter)
+  }
+  const removeValue=()=>{
+    if (counter<=0){
+      return 
+    }
+    counter-=1
+    setCounter(counter)
+  }
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter Value:{counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}>Remove Value:</button>
+    </>
+  )
+}
+export default App
