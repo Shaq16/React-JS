b6b560e948afbe8e6361c9244b665da287dc7719
Shakthi <shakthi.jashwanth@gmail.com>
2025-08-02 09:51:10 +0530

    React under the hood

---
+++ customReact/customreact.js
@@ -0,0 +1,29 @@
+function customRender(element,container){
+   /*
+    const domElement = document.createElement(element.type)
+    domElement.innerHTML= element.children
+    domElement.setAttribute('href',element.props.href)
+    domElement.setAttribute('target',element.props.target)
+    container.appendChild(domElement)
+    */
+    const domElement = document.createElement(element.type)
+    domElement.innerHTML= element.children
+    for(const prop in reactElement.props){
+        if (prop==='children') continue
+        domElement.setAttribute(prop,reactElement.props[prop])
+
+    }
+    container.appendChild(domElement)
+}
+
+const container = document.querySelector("#root")
+const reactElement = {
+    type:'a',
+    props:{
+        href:"https://google.com",
+        target:'_blank'
+    },
+    children:'Click me to visit Google'
+}
+
+customRender(reactElement,container)
