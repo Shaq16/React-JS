03aa997a3d0b759a3e646e02bd675f10cc2739e8
Shakthi <shakthi.jashwanth@gmail.com>
2025-10-08 21:18:08 +0530

    Practice

---
+++ currencyconverter/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-orange-500 text-3xl flex justify-center mt-5 '>Currency Converter App</div>
+    </>
+  )
+}
+
+export default App
