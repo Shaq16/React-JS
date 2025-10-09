114895acb27d455241ed30ada5a61ebdef316427
Shakthi <shakthi.jashwanth@gmail.com>
2025-10-09 07:12:59 +0530

    Updated links to match routing

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 import { Routes, Route } from 'react-router'
+import { CheckoutPage } from './pages/CheckoutPage'
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
-      <Route path="checkout" element={<div>Test</div>}></Route>
+      <Route path="checkout" element={<CheckoutPage/>}></Route>
     </Routes>
   )
 }
